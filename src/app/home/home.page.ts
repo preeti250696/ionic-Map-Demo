0221bfdaa28216e875cc304ecbe96b26952a426d
preeti250696 <preeti.liaf@gmail.com>
2020-11-17 21:11:02 +0530

    initialize map

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
-
+import {ViewChild, ElementRef} from '@angular/core';
+declare var google:any;
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  map: any;
+  @ViewChild('map',{read: ElementRef,static:false}) mapRef:ElementRef;
   constructor() {}
-
+  ionViewDidEnter(){
+    this.showMap();
+  }
+  showMap(){
+    const location = new google.maps.LatLng(27.897394,78.088013);
+    const options = {
+      center: location,
+      zoom: 15,
+      disableDefaultUI: true
+    }
+    this.map = new google.maps.Map(this.mapRef.nativeElement, options);
+  }
 }
