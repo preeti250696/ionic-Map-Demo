fc6745bdde348370d1e976e139487e2066b1a7b3
preeti250696 <preeti.liaf@gmail.com>
2020-11-18 11:32:12 +0530

    added markers to the map

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,9 +10,67 @@ declare var google: any;
 export class HomePage {
   map: any;
   @ViewChild('map', { read: ElementRef, static: false }) mapRef: ElementRef;
+  infoWindows:any = [];
+  markers: any = [
+    {
+      title: 'Nashik',
+      latitude: '20.125931',
+      longitude:'73.780271'
+    },
+    {
+     title: 'Aligarh Muslim University',
+     latitude:'27.904110',
+     longitude:'78.076170',
+    },
+    {
+      title: 'Lucknow',
+      latitude:'26.690656',
+      longitude:'80.992655'
+    },
+    {
+      title:'New Delhi',
+      latitude:'28.694443',
+      longitude: '77.496670'
+    }
+  ]
   constructor(private geolocation: Geolocation) { }
   ionViewDidEnter() {
     this.showMap();
+  }
+  addMarkersToMap(markers){
+    for(let marker of markers){
+      let position = new google.maps.LatLng(marker.latitude,marker.longitude);
+      let mapMarker = new google.maps.Marker({
+        position: position,
+        title: marker.title,
+        latitude: marker.latitude,
+        longitude: marker.longitude
+      });
+      mapMarker.setMap(this.map);
+      this.addInfoWindowToMarker(mapMarker);
+    }
+  }
+  addInfoWindowToMarker(marker){
+    let infoWindowContent = '<div id="content">' +
+                             '<h2 id="firstHeading class="firstHeading>'+ marker.title + '</h2>' +
+                             '<p>Latitude: ' + marker.latitude + '</p>' +
+                             '<p>Longitude:' + marker.longitude + '</p>'+
+                             '</div>';
+        
+    let infoWindow = new google.maps.InfoWindow({
+      content: infoWindowContent
+    });
+    marker.addListener('click',()=>{
+      this.closeAllInfoWindows();
+      infoWindow.open(this.map, marker);
+    });                  
+    this.infoWindows.push(infoWindow);
+    console.log(this.infoWindows,'infowindows');
+  }
+  closeAllInfoWindows(){
+    for(let window of this.infoWindows){
+      window.close();
+    }
   }
   showMap() {
     this.geolocation.getCurrentPosition().then((position) => {
@@ -23,6 +81,7 @@ export class HomePage {
         disableDefaultUI: true
       }
       this.map = new google.maps.Map(this.mapRef.nativeElement, options);
+      this.addMarkersToMap(this.markers);
     })
   }
 }
